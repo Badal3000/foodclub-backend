6223eeb26d228d0cb1f592dc9a6c5547fea43bb9
Badal3000 <badal.chaurasia1234@gmail.com>
2025-01-10 16:28:01 +0530

    resolving bcrypt

--- utils/hash.js
+++ utils/hash.js
@@ -1,4 +1,4 @@
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 
 exports.hashPassword = (password) =>{
     return bcrypt.hashSync(password, Number(process.env.SALT_ROUNDS) || 10);
